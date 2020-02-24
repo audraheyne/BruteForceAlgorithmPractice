f021688e79505f42227a2434fdab8c60de7f0f42
Audra Heyne <42616087+audraheyne@users.noreply.github.com>
2020-02-24 10:19:28 -0600

    Update FileDistance

--- FileDistance/js/index.js
+++ FileDistance/js/index.js
@@ -14,6 +14,64 @@ var tokenize = (text) => {
 	return text;
 }
 
+/*
+ *
+ * This function returns the shortest distance (in numbers of words) between
+ *  two search words. This function does the computation, while the above changes the input.
+ */
+var minDistanceHelper = (words, searchWords) => {
+    let minimumDistance = Number.MAX_SAFE_INTEGER
+    let currDistance = 0
+    let word1Exists = false
+    let word2Exists = false
+ 
+    for (let i = 0; i < text.length - Math.max(searchWords[0].length, searchWords[1].length); i++) {
+        if(!word1Exists){
+            word1Exists = doesWordExistAtIndex(text, searchWords[0], i)
+            currDistance = 0;
+        }
+ 
+        if (text[i] === " ") {
+            currDistance++;
+        }
+ 
+        if(!word2Exists && word1Exists){
+            word2Exists = doesWordExistAtIndex(text, searchWords[1], i)
+            // Gets rid of the current word because the next word is found!
+            if(word2Exists) currDistance--;
+        }
+ 
+        if(word1Exists && word2Exists){
+            // Check to see if it's the minimum and then reset to false
+            if (minimumDistance > currDistance) {
+                minimumDistance = currDistance
+            }
+            word1Exists = false
+            word2Exists = false
+        }
+    }
+    return minimumDistance
+}
+
+
+/**
+ * This function checks to see if a word exists at a given index in an array.
+ * @param {Array} text the text to search through
+ * @param {string} searchWord  the word to find
+ * @param {number} index the index in the text array
+ */
+var doesWordExistAtIndex = (text, searchWord, index) => {
+    let wordMatches = true
+    for(let i = 0; i < searchWord.length; i++){
+        if(text[index + i] !== searchWord[i]){
+            return false
+        }
+    }
+    return wordMatches
+}
+ 
+
+
 
 /*
  * This function returns the shortest distance (in terms of number of words) between 
@@ -22,8 +80,14 @@ var tokenize = (text) => {
  */
 var minDistance = (text,searchWords) => {
 	words = tokenize(text);
-	// TODO
-	return undefined;
+ 
+    // Get distances from both combination and return the lowest
+    let firstCombinationDistance = minDistanceHelper(words, searchWords)
+ 
+    let swappedSearchWords = Array(searchWords[1], searchWords[0])
+    let secondCombinationDistance = minDistanceHelper(words, swappedSearchWords)
+ 
+    return Math.min(firstCombinationDistance, secondCombinationDistance)
 }
 
 
